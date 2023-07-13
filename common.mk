7661553cc633df51085ce4dbd0d1a258e4eea46b
Demon000 <demonsingur@gmail.com>
2023-07-13 21:31:30 +0800

    sm8350-common: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- common.mk
+++ common.mk
@@ -104,6 +104,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    libudfpshandler \
+    sensors.xiaomi
+
+PRODUCT_PACKAGES += \
+    FrameworkOverlayUDFPS
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
