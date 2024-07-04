dcf00130538611acbf0b6dbf9bcad0b52d59faa7
xNombre <linux@andrzejperczak.com>
2024-07-04 23:12:41 +0800

    lahaina: Symlink libvndfwk_detect_jni.qti for CneApp Needed by CneApp.
    dlopen failed: linker  : library "/system_ext/lib64/libcamera_algoup_jni.xiaomi.so" ("/system_ext/lib64/libcamera_algoup_jni.xiaomi.so") needed or dlopened by "/apex/com.android.art/lib64/libnativeloader.so" is not accessible for the namespace: [name="product-clns-4", ld_library_paths="", default_library_paths="/product/priv-app/MiuiCamera/MiuiCamera.apk!/lib/arm64-v8a", permitted_paths="/data:/mnt/expand:/data/data/com.android.camera"]
    
    Change-Id: I400f03c3cb60acdc4263c24b1096c5961142f03f

--- Android.mk
+++ Android.mk
@@ -69,4 +69,14 @@ $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WIFI_FIRMWARE_SYMLINKS)
 
+CNE_LIBS := libcamera_algoup_jni.xiaomi.so
+CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBS)))
+$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "CNE lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS) 
+
 endif
