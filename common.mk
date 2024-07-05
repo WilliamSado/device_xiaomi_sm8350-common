b7e0f03d086e3843e8bb64fa1e0cc77c0f8770bf
WilliamSado <williamsado1004@gmail.com>
2024-07-06 07:49:13 +0800

    sm8350-common: Build libion.vendor

--- common.mk
+++ common.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     android.hardware.graphics.allocator@3.0  \
-    libutilscallstack.vendor
+    libutilscallstack.vendor \
+    libion.vendor
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
