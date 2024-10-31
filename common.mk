7fecc251f58fdf96c588d293b84bdf587aa533af
Michael Bestas <mkbestas@lineageos.org>
2024-10-31 15:32:55 +0100

    sm8350-common: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- common.mk
+++ common.mk
@@ -74,7 +74,8 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    libcrypto-v33
 
 # Fastbootd
 PRODUCT_PACKAGES += \
