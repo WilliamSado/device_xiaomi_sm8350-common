4f7d98777cf3cefb8c740c1a9151e2c2d8423029
bgcngm <bgcngm@gmail.com>
2025-01-18 23:24:15 +0800

    sm8350-common: Mark CNE symlink target as required package

--- Android.mk
+++ Android.mk
@@ -11,14 +11,4 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-CNE_LIBS := libcamera_algoup_jni.xiaomi.so
-CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBS)))
-$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "CNE lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS) 
-
 endif
