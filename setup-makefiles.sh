09f7e207376c8b198bf834a192f240016c4ad6db
WilliamSado <williamsado1004@gmail.com>
2025-02-14 17:27:16 +0800

    sm8350-common: Fixup some blobs

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,6 +48,9 @@ function lib_to_package_fixup_vendor_variants() {
             com.qualcomm.qti.imscmservice* | \
             com.qualcomm.qti.uceservice* | \
             libmmosal | \
+            libmegface | \
+            libqcbor | \
+            libminksocket | \
             vendor.qti.data.* | \
             vendor.qti.diaghal@1.0 | \
             vendor.qti.hardware.data.* | \
