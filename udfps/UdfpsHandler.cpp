84eb1060845808956f87d8fd4e11151b8bc76b49
WilliamSado <williamsado1004@gmail.com>
2025-02-14 18:16:49 +0800

    sm8350-common: Switch to AIDL fingerprint

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -93,18 +93,6 @@ class XiaomiUdfpsHander : public UdfpsHandler {
         // nothing
     }
 
-    void preEnroll() {
-        // nothing
-    }
-
-    void enroll() {
-        // nothing
-    }
-
-    void postEnroll() {
-        // nothing
-    }
-
     void onAcquired(int32_t result, int32_t vendorCode) {
         if (result == FINGERPRINT_ACQUIRED_GOOD) {
             set(FOD_HBM_PATH, FOD_HBM_OFF);
