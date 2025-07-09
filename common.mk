733a9e8f5f2f89f26401e7754a5cffb2b156b50e
WilliamSado <williamsado1004@gmail.com>
2025-07-09 15:42:56 +0800

    sm8350-common: Build AIDL clearkey

--- common.mk
+++ common.mk
@@ -93,7 +93,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
 # Fastbootd
