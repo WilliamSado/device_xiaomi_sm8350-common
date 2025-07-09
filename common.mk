0c860b28b5673d2653736026717476df28457ed6
WilliamSado <williamsado1004@gmail.com>
2025-07-09 15:42:02 +0800

    sm8350-common: Build some missing QPR3 packages

--- common.mk
+++ common.mk
@@ -76,6 +76,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    liblz4.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
 
